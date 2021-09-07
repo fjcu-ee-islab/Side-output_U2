a1127ba47bb7afb90880d183a5bb658d64e9b36f
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 07:35:55 +0800

    Update train_side_sl.sh

--- train_side_sl.sh
+++ train_side_sl.sh
@@ -14,6 +14,6 @@ CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 tr
 	--stride 32 \
     --lr_milestones 350 450 \
 	--epochs 500 \
-	--name side_sl \
+	--name train_xray_large \
 	--train_file /home/ubuntu/Downloads/slomo_mine/data/xray_large/train \
 	--val_file /home/ubuntu/Downloads/slomo_mine/data/xray_large/val \
