e215ce35af97df925cbd42849a7a72a1aa3b98bf
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 07:34:21 +0800

    Update train_adobe.sh

--- train_adobe.sh
+++ train_adobe.sh
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 tr
 	--stride 32 \
     --lr_milestones 350 450 \
 	--epochs 500 \
-    --name u2_sl_adobe \
+    --name train_adobe \
     --train_file /home/ubuntu/Downloads/slomo_mine/data/adobe/train \
     --val_file /home/ubuntu/Downloads/slomo_mine/data/adobe/validation \
 
