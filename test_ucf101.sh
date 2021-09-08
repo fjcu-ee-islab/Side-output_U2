30448cf04de6c1258c97820062472c8378c2529e
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 09:17:18 +0800

    Update test_ucf101.sh

--- test_ucf101.sh
+++ test_ucf101.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=1 python3 eval_other.py \
 	--flow_scale 1 \
 	--dataset VideoInterp \
 	--val_file /home/ubuntu/Downloads/slomo_mine/data/ucf101/test \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/u2_sl_ucf101.tar \
+    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/ucf101.tar \
     --val_batch_size 1 \
     --gpus 1 \
     --name ucf101_result \
