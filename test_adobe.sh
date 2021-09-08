94b0c304530fb7304d31cee495d659d7a64019d3
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 09:30:34 +0800

    Update test_adobe.sh

--- test_adobe.sh
+++ test_adobe.sh
@@ -4,8 +4,8 @@ CUDA_VISIBLE_DEVICES=1 python3 eval_other.py \
 	--num_interp 1 \
 	--flow_scale 1 \
 	--dataset VideoInterp \
-	--val_file /home/ubuntu/Downloads/slomo_mine/data/adobe/adobe_test/test_1 \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/adobe.tar \
+	--val_file /home/ubuntu/Downloads/Side-output_U2-main/data/adobe/adobe_test/test_1 \
+    --resume /home/ubuntu/Downloads/Side-output_U2-main/pretrained_models/adobe.tar \
     --val_batch_size 1 \
     --gpus 1 \
     --name adobe_result_1 \
@@ -18,8 +18,8 @@ CUDA_VISIBLE_DEVICES=1 python3 eval_other.py \
 	--num_interp 3 \
 	--flow_scale 1 \
 	--dataset VideoInterp \
-	--val_file /home/ubuntu/Downloads/slomo_mine/data/adobe/adobe_test/test_3 \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/dobe.tar \
+	--val_file /home/ubuntu/Downloads/Side-output_U2-main/data/adobe/adobe_test/test_3 \
+    --resume /home/ubuntu/Downloads/Side-output_U2-main/pretrained_models/dobe.tar \
     --val_batch_size 1 \
     --gpus 1 \
     --name adobe_result_3 \
@@ -32,8 +32,8 @@ CUDA_VISIBLE_DEVICES=1 python3 eval_other.py \
 	--num_interp 7 \
 	--flow_scale 1 \
 	--dataset VideoInterp \
-	--val_file /home/ubuntu/Downloads/slomo_mine/data/adobe/adobe_test/test_7 \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/adobe.tar \
+	--val_file /home/ubuntu/Downloads/Side-output_U2-main/data/adobe/adobe_test/test_7 \
+    --resume /home/ubuntu/Downloads/Side-output_U2-main/pretrained_models/adobe.tar \
     --val_batch_size 1 \
     --gpus 1 \
     --name adobe_result_7 \
