57ca0fe34ccc13af6c02e18e39b2c198194516ed
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 09:18:55 +0800

    Update test_xray_single.sh

--- test_xray_single.sh
+++ test_xray_single.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=1 python3 eval.py \
 	--flow_scale 1 \
 	--dataset VideoInterp \
 	--val_file /home/ubuntu/Downloads/slomo_mine/data/xray_large/test \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/side_sl.tar \
+    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/xray_large.tar \
     --val_batch_size 1 \
     --gpus 1 \
 	--name xray_large \
@@ -17,8 +17,8 @@ CUDA_VISIBLE_DEVICES=0 python3 eval.py \
 	--num_interp 1 \
 	--flow_scale 1 \
 	--dataset VideoInterp \
-	--val_file /home/ubuntu/backup/mine/data_train/test \
-    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/utwo_sl_window_large.tar \
+	--val_file /home/ubuntu/Downloads/slomo_mine/data/xray_large/test \
+    --resume /home/ubuntu/Downloads/slomo_mine/pretrained_models/xray_large_window.tar \
 	--name window_u2_sl \
     --val_batch_size 1 \
     --gpus 1 \
