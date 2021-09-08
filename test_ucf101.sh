b86339d95982900964926b741851018042810418
allenintw <89689482+allenintw@users.noreply.github.com>
2021-09-08 10:08:14 +0800

    Update test_ucf101.sh

--- test_ucf101.sh
+++ test_ucf101.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=1 python3 eval_other.py \
     --resume /home/ubuntu/Downloads/Side-output_U2-main/pretrained_models/ucf101.tar \
     --val_batch_size 1 \
     --gpus 1 \
-    --name ucf101_result \
+    --name test_ucf101_result \
     --write_images \
 
 
